190905f087cdd237592eafa065206760e38d1396
Pavan_kumar <cpavankumar2580@gmail.com>
2026-04-22 21:11:50 +0530

    Add project files.

---
+++ TaskFlow.Domain/Common/BaseEntity.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskFlow.Domain.Common
+{
+    public abstract class BaseEntity // Public -  Means accessible from any assemly. Abstract - Cannot be instantiated directly, but can be inherited by other classes.
+    {
+        public Guid Id { get; protected set; } // Global Unique Identifier (GUID) - A unique identifier for each entity. Protected set - Can only be set within the class or by derived classes, ensuring controlled access to the Id property.
+        public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow; // Date and time when the entity was created. Protected set - Can only be set within the class or by derived classes.
+
+        public DateTime? UpdatedAt { get; protected set; } // Date and time when the entity was last updated. Nullable - ? indicates that the property can have a null value, which is useful for entities that have not been updated since creation. 
+        public string CreatedBy { get; protected set; } = string.Empty;// Identifier of the user who created the entity. Protected set - Can only be set within the class or by derived classes.
+        public bool IsDeleted { get; protected set; } = false; // IsDeleted - Soft delete  instead  of hard deleting the record from the database, we mark it as deleted. This allows us to retain the data for historical or auditing purposes while preventing it from being accessed in normal operations. 
+
+        public void MarkAsUpdated()
+        {
+            UpdatedAt = DateTime.UtcNow; // Update the UpdatedAt property to the current UTC time when the entity is modified.
+
+        }
+        public void SoftDelete()
+        {
+
+            IsDeleted = true; // Mark the entity as deleted by setting IsDeleted to true. This allows us to retain the data while preventing it from being accessed in normal operations.
+            MarkAsUpdated(); // method composition - one method calling another. this ensures UpdateAt is always set when soft deleting an entity, maintaining accurate tracking of when the entity was modified.}
+
+        }
+
+
+    }
+}
---
+++ TaskFlow.Domain/Enum/TaskEnum.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Runtime.Intrinsics.X86;
+using System.Runtime.Serialization;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TaskFlow.Domain.Enum
+{
+    internal class TaskEnum
+    {
+        public enum Priority // Enum - A distinct type that consists of a set of named constants called the enumerator list. In this case, Priority is an enumeration that defines different levels of task priority.
+        {
+            Low = 1,
+            Medium = 2,
+            High = 3,
+            Critical = 4
+        }
+
+        public enum TaskStaus // •	An enum (short for "enumeration") is a named list of related constant values. 2. Use it when a variable should only hold one value from a fixed set (e.g., priority levels or status).Enums make code clearer and less error - prone than using raw numbers or strings.
+        {
+            Todo=1,       //will let us know where the task is in the workflow, whether it's still to be done, in progress, under review, completed, or cancelled. This can help us manage and track the lifecycle of tasks effectively.
+            InPrgress =2,
+            InReview=3,
+            Done= 4 ,
+            Cancelled=5
+
+        }
+
+        public enum ProjectStatus // what state our project is in.
+        {
+            Active = 1,
+            OnHold=2,
+            Completed=3,
+            Archived= 4
+        } 
+        public enum TenantPlan // subscription plan of the company using our app
+        {
+            Free = 1,
+            Pro = 2,
+            Enterprise = 3
+        }
+
+
+
+
+
+
+    }
+}
